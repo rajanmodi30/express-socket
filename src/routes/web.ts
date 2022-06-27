518d08691b25a10c4aaf75ceb52fcb7ff37702f4
rajan <rajanmodi30@gmail.com>
2022-06-27 22:05:28 +0530

    -removed one route

--- src/routes/web.ts
+++ src/routes/web.ts
@@ -2,20 +2,20 @@ import { Request, Router, Response } from "express";
 import path from "path";
 const webRouter = Router();
 
+webRouter.get("/", (req: Request, res: Response) => {
+  return res.json({
+    success: true,
+    message: "hello world",
+  });
+});
+
 webRouter.get("/chat", (req: Request, res: Response) => {
   if (process.env.APP_ENV === "production") {
-    return res.json({
+    return res.status(400).json({
       success: false,
     });
   }
   return res.sendFile(path.join(__dirname + "/../views/index.html"));
-});
-
-webRouter.get("/ping", (req: Request, res: Response) => {
-  res.json({
-    success: true,
-    message: "pong",
-  });
 });
 
 export default webRouter;
